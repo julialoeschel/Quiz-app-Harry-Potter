2c69ad7471a2d2fa49f7393d4e9bacb8e7e1d5d9
Julia Löschel <juloeschel@gmail.com>
2022-02-16 20:20:33 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const createAddQuestion = document.querySelector("[data-js=text-addquestion]");
+const toggleBookmark = document.querySelector("[data-js=bookmark]");
+
+createAddQuestion.addEventListener("input", () => {
+  const questionTextLength = createAddQuestion.value.length;
+  console.log(questionTextLength);
+});
+
+toggleBookmark.addEventListener("click", () => {
+  toggleBookmark.classList.toggel("booked");
+});
