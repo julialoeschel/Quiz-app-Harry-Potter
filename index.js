9e764107f851201408d26f0bcb69413baf4b6310
Julia Löschel <juloeschel@gmail.com>
2022-02-17 12:57:39 +0100

    install esLint, create tests

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const createAddQuestion = document.querySelector("[data-js=text-addquestion]");
-const toggleBookmark = document.querySelector("[data-js=bookmark]");
+const createAddQuestion = document.querySelector('[data-js=text-addquestion]');
+const toggleBookmark = document.querySelector('[data-js=bookmark]');
 
-createAddQuestion.addEventListener("input", () => {
+createAddQuestion.addEventListener('input', () => {
   const questionTextLength = createAddQuestion.value.length;
   console.log(questionTextLength);
 });
 
-toggleBookmark.addEventListener("click", () => {
-  toggleBookmark.classList.toggel("booked");
+toggleBookmark.addEventListener('click', () => {
+  toggleBookmark.classList.toggel('booked');
 });
