550a61d77c5d5652aad5af4f169c29ab9a816b70
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-18 14:44:55 +0100

    Refactor with function imports

--- index.js
+++ index.js
@@ -1,81 +1,14 @@
-const cardSet = document.querySelectorAll("[data-js=card]");
+// import { cards, cardBookmark } from "./js/cards.js";
+import cards from "./js/cards.js";
+import navigation from "./js/navigation.js";
+import counter from "./js/counter.js";
 
-cardSet.forEach((card) => {
-  const bookmark = card.querySelector("[data-js=bookmark]");
-  const answerButton = card.querySelector("[data-js=answerButton]");
-  const answerText = card.querySelector("[data-js=answerText]");
-  const answerSpanHidden = card.querySelector("[data-js=answerSpanHidden]");
-  const answerHeading = card.querySelector("[data-js=answerHeading]");
+cards();
 
-  bookmark.addEventListener("click", () => {
-    bookmark.classList.toggle("bookmark--booked");
-  });
+/* ---------------------- */
 
-  answerButton.addEventListener("click", () => {
-    answerSpanHidden.classList.toggle("hidden");
-    answerHeading.classList.toggle("hidden");
-    answerText.classList.toggle("hidden");
-  });
-});
+navigation();
 
-const homeSection = document.querySelector("[data-js=cards-section-home]");
-const bookmarkSection = document.querySelector(
-  "[data-js=cards-section-bookmark]"
-);
-const profileSection = document.querySelector(
-  "[data-js=cards-section-profile]"
-);
+/* ---------------------- */
 
-const addSection = document.querySelector("[data-js=cards-section-add]");
-const bookmarkNav = document.querySelector("[data-js=nav-bookmark]");
-const homeNav = document.querySelector("[data-js=nav-home]");
-const addNav = document.querySelector("[data-js=nav-add]");
-const profileNav = document.querySelector("[data-js=nav-profile]");
-
-bookmarkNav.addEventListener("click", () => {
-  bookmarkSection.classList.remove("hidden");
-  homeSection.classList.add("hidden");
-  addSection.classList.add("hidden");
-  profileSection.classList.add("hidden");
-});
-
-homeNav.addEventListener("click", () => {
-  bookmarkSection.classList.add("hidden");
-  homeSection.classList.remove("hidden");
-  addSection.classList.add("hidden");
-  profileSection.classList.add("hidden");
-});
-
-addNav.addEventListener("click", () => {
-  addSection.classList.remove("hidden");
-  bookmarkSection.classList.add("hidden");
-  homeSection.classList.add("hidden");
-  profileSection.classList.add("hidden");
-});
-
-profileNav.addEventListener("click", () => {
-  addSection.classList.add("hidden");
-  bookmarkSection.classList.add("hidden");
-  homeSection.classList.add("hidden");
-  profileSection.classList.remove("hidden");
-});
-
-const createAddQuestion = document.querySelector("[data-js=text-addquestion]");
-const amountLeftAddQuestion = document.querySelector(
-  "[data-js=create-amount-add-question]"
-);
-const createAddAnswer = document.querySelector("[data-js=text-addanswer]");
-const amountLeftAddAnswer = document.querySelector(
-  "[data-js=create-amount-add-answer]"
-);
-
-createAddQuestion.addEventListener("input", () => {
-  const questionTextLength = createAddQuestion.value.length;
-  const maxLengthAddQuestion = createAddQuestion.maxLength;
-  amountLeftAddQuestion.innerText = maxLengthAddQuestion - questionTextLength;
-});
-
-createAddAnswer.addEventListener("input", () => {
-  amountLeftAddAnswer.innerText =
-    createAddAnswer.maxLength - createAddAnswer.value.length;
-});
+counter();
