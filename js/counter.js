b52318b87d81a874df00224e8ba6e94ba555f39d
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-18 14:43:32 +0100

    Refactor counter functionality

---
+++ js/counter.js
@@ -0,0 +1,25 @@
+function counter() {
+  const createAddQuestion = document.querySelector(
+    "[data-js=text-addquestion]"
+  );
+  const amountLeftAddQuestion = document.querySelector(
+    "[data-js=create-amount-add-question]"
+  );
+  const createAddAnswer = document.querySelector("[data-js=text-addanswer]");
+  const amountLeftAddAnswer = document.querySelector(
+    "[data-js=create-amount-add-answer]"
+  );
+
+  createAddQuestion.addEventListener("input", () => {
+    const questionTextLength = createAddQuestion.value.length;
+    const maxLengthAddQuestion = createAddQuestion.maxLength;
+    amountLeftAddQuestion.innerText = maxLengthAddQuestion - questionTextLength;
+  });
+
+  createAddAnswer.addEventListener("input", () => {
+    amountLeftAddAnswer.innerText =
+      createAddAnswer.maxLength - createAddAnswer.value.length;
+  });
+}
+
+export default counter;
